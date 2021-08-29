8b208e4e15050eab9fef402d942dfe5b5f5239e6
Wences81 <zhylinpochta@gmail.com>
2021-08-29 19:47:03 +0200

    day3.8

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -3,7 +3,14 @@ import PropTypes from 'prop-types';
 
 
 
+const colorPicker = () => {
+  const red = Math.round(Math.random() * 255);
+  const green = Math.round(Math.random() * 255);
+  const blue = Math.round(Math.random() * 255);
+  const backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 
+  return backgroundColor;
+};
 
 export default function Statistics({ title, stats }) {
   return (
@@ -12,7 +19,7 @@ export default function Statistics({ title, stats }) {
 
       <ul className={s.statList}>
         {stats.map(stat => (
-          <li className={s.item} key={stat.id}>
+          <li className={s.item} key={stat.id} style={{ backgroundColor: colorPicker() }}>
             <span className={s.label}>{stat.label}</span>
             <span className={s.percentage}>{stat.percentage} %</span>
           </li>
